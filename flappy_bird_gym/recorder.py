30abc03988368a0c423ccfadaa007b3a27788b5d
Samuel Boylan-Sajous <35412345+samboylan11@users.noreply.github.com>
2021-08-17 17:24:02 -0400

    fixes

--- flappy_bird_gym/recorder.py
+++ flappy_bird_gym/recorder.py
@@ -70,7 +70,7 @@ class Recorder(gym.Wrapper):
         start = time.time()
         filename = 'temp-{start}.mp4'
         clip = VideoFileClip(self.path)
-        clip.rotate(180)
+        clip = clip.fx(vfx.mirror_x)
         clip.write_videofile(filename, progress_bar = False, verbose = False)
         display(Video(filename, embed = True))
         os.remove(filename)
